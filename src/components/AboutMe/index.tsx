a64e09621acb4c9504684186a989a440d57bd6df
Bruno Araújo <bruno.ar.avl@gmail.com>
2023-11-13 21:41:11 -0300

    feat: about me component

---
+++ src/components/AboutMe/index.tsx
@@ -0,0 +1,40 @@
+const AboutMe = () => {
+  return (
+    <>
+      <h1>Sobre mim</h1>
+      <p>
+        Me chamo Bruno Araujo, sou desenvolvedor web desde 2014. Atualmente, sou
+        sócio da Envoy Tecnologia e atuo como Tech Lead. Além disso, faço parte
+        do time de voluntários do Somando mais Ações atuando na área de
+        tecnologia.
+      </p>
+      <p>
+        Comecei na área logo cedo, aos 13 anos. Sempre tive curiosidade sobre os
+        mais diversos assuntos relacionados a tecnologia. Comecei com manutenção
+        de computadores até chegar numa pergunta chave que me fez ir para o
+        caminho do desenvolvimento de software: Como os softwares e sistemas são
+        feitos? Desde então, passei a estudar mais sobre desenvolvimento de
+        sistemas. Comecei fazendo curso técnico no final do ensino médio e em
+        2016 me formei em Análise e Desenvolvimento de Sistemas.
+      </p>
+      <p>
+        Atualmente venho buscando aprofundar meus conhecimentos sobre a base da
+        computação, novas linguagens, redes e cloud. Ainda hoje, novos
+        conhecimentos sobre tecnologia me fascinam, por isso criei esse site
+        para compartilhar parte da minha experiência e conhecimento e também
+        para aprender com vocês.
+      </p>
+      <p>
+        Além da tecnologia, gosto bastante de política internacional,
+        astrônomia, história e ciências. Sou viciado em Counter Strike e The
+        Witcher.
+      </p>
+      <p>
+        Meu contato profissional:{' '}
+        <a href="mailto:bruno.ar.avl@gmail.com">bruno.ar.avl@gmail.com</a>
+      </p>
+    </>
+  )
+}
+
+export default AboutMe
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import AboutMe from 'components/AboutMe'
+import Base from 'components/Base'
+
+const AboutMePage = () => {
+  return (
+    <Base>
+      <AboutMe></AboutMe>
+    </Base>
+  )
+}
+
+export default AboutMePage
